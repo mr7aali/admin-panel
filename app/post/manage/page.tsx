ac6fc23b20728f357a09bb116a3bf064b1d149b8
mr7aali <mr7aali@gmail.com>
2023-12-26 23:45:43 +0600

    first commit

---
+++ app/post/manage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManagePost = () => {
+    return (
+        <div>
+            <h1>This is mangage post page</h1>
+        </div>
+    );
+};
+
+export default ManagePost;
